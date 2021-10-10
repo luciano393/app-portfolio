fd62bc3757073d8a66bf598db827f25219fe78cd
luciano393 <luciano39340268@gmail.com>
2021-10-10 12:42:46 -0300

    Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import foto from '../assets/images/foto.png'
 import { ButtonComponent } from './ButtonComponent'
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
     return (
@@ -8,10 +9,12 @@ export const Home = () => {
             <div className="HomeWrapper">
                 <img src={foto} alt=""/>
                 <h1>Luciano <span>Ibarra</span></h1>
-                <p>I'am <span>full stack developer</span></p>
-                <ButtonComponent className="btn primary"
-                text="About me"
-                />
+                <p>I'am <span>front end developer</span></p>
+                <Link to="/about">
+                    <ButtonComponent className="btn primary"
+                    text="About me"
+                    />
+                </Link>
             </div>
         </div>
     )
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ export const About = () => {
             <section className="About">
                 <Heading text="About" span="Me"/>
                 <div className="box-container">
-                    <h3>Hi! I'm a <span>full stack web developer</span></h3>
+                    <h3>Hi! I'm a <span>front end developer</span></h3>
                     <p>I like challenges and testing my knowledge. I have a great passion for web development and web applications. I work with the JavaScript programming language and technologies based on it.</p>
                     <p>I have knowledge of Mongo DB, Express js, Node js, and React js. </p>
                 </div>
